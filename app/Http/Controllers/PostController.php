7f952dce7366c149bfbff72e933d0a7ce5ef8745
sassy0124 <satoshi01060373@gmail.com>
2022-07-30 10:35:22 +0900

    rooting_sample_1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post) {
-        return $post->get();
+        return view ('posts/index')->with(['posts' => $post->get()]);
     }
 }

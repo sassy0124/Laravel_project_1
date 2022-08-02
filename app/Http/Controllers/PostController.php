bd39c41f24df67e721e63a1342de5704b0c40953
sassy0124 <satoshi01060373@gmail.com>
2022-08-02 19:21:21 +0900

    リクエストパラメータの取得とDBへの登録

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,9 @@ class PostController extends Controller
     public function show(Post $post) {
         return view('posts/show')->with(['post' => $post]);
     }
+    public function store(Request $request, Post $post){
+        $input=$request['post'];        //postをキーにもつリクエストパラメータを取得（$requestのキーは、HTMLのFormタグ内で定義した各入力項目のname属性と一致）
+        $post->fill($input)->save();        //先ほどまで空だったPostインスタンスのプロパティを、受け取ったキーごとに上書き。$post->title はタイトル、$post->bodyは本文。
+        return redirect('/posts/' . $post->id);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@
 Route::get('/posts', 'PostController@index');
 Route::get('posts/create', 'PostController@create');
 Route::get('/posts/{post}', 'PostController@show');
+Route::post('/posts', 'PostController@store');
 

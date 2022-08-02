9159011aa978731b5f2fd575874cb4ccab280709
sassy0124 <satoshi01060373@gmail.com>
2022-08-03 01:25:46 +0900

    validationの追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Post;
 
 class PostController extends Controller
@@ -16,7 +16,7 @@ class PostController extends Controller
     public function show(Post $post) {
         return view('posts/show')->with(['post' => $post]);
     }
-    public function store(Request $request, Post $post){
+    public function store(PostRequest $request, Post $post){
         $input=$request['post'];        //postをキーにもつリクエストパラメータを取得（$requestのキーは、HTMLのFormタグ内で定義した各入力項目のname属性と一致）
         $post->fill($input)->save();        //先ほどまで空だったPostインスタンスのプロパティを、受け取ったキーごとに上書き。$post->title はタイトル、$post->bodyは本文。
         return redirect('/posts/' . $post->id);

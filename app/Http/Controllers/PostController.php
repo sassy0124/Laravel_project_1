9d4fc6bfeb921676c22b5375342704618516c0ae
sassy0124 <satoshi01060373@gmail.com>
2022-08-03 22:48:55 +0900

    編集機能の追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,7 +13,15 @@ class PostController extends Controller
     public function create() {
         return view ('posts/create');
     }
-    public function show(Post $post) {
+     public function edit(Post $post) {
+        return view ('posts/edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post){
+        $input=$request['post'];        //postをキーにもつリクエストパラメータを取得（$requestのキーは、HTMLのFormタグ内で定義した各入力項目のname属性と一致）
+        $post->fill($input)->save();        //先ほどまで空だったPostインスタンスのプロパティを、受け取ったキーごとに上書き。$post->title はタイトル、$post->bodyは本文。
+        return redirect('/posts/' . $post->id);
+    }
+     public function show(Post $post) {
         return view('posts/show')->with(['post' => $post]);
     }
     public function store(PostRequest $request, Post $post){
